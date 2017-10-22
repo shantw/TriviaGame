c99ac7b27d22b27bc972318e5999891b7f9ced42
Shant Wanes <swanes@bjsrestaurants.com>
2017-10-22 14:41:30 -0700

    almost there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,59 +2,133 @@ var questions = [{q: "What year was the National Basketball Association first es
 				  {q: "Michael Jordan was drafted third overall in 1984. Which two players were selected ahead of him?", a :["Patrick Ewing & Hakeem Olajuwan","Charles Barkley & Hakeem Olajuwan", "Karl Malone & Sam Bowie"],rightAnswer:"Hakeem Olajuwan & Sam Bowie"},
 				  {q: "What year did the NBA-ABA merger occur?", a:["1970","1973","1974"],rightAnswer:"1976"},
 				  {q: "Which country has won the World Cup 5 times?", a:[ "Germany","Italy","England"], rightAnswer:"Brazil"},
-				  {q: "Which country has won the World Cup 5 times?", a:[ "Spain","Italy","France"], rightAnswer:"Netherlands"},
-				  {q: "Which country has won the World Cup 5 times?", a:["Brazil","Cameroon","Argentina"], rightAnswer:"Ghana"}
+				  {q: "Which nation has reached the World Cup final three times, but never won?", a:[ "Spain","Italy","France"], rightAnswer:"Netherlands"},
+				  {q: "Which team was the first from Africa to reach the quarterfinals of a World Cup?", a:["Brazil","Cameroon","Argentina"], rightAnswer:"Ghana"}
 ];
 var time = 0;
 var intervalId;
 var gameStatus = "new";
-var questionTime = 45;
+var questionTime = 7;
 var qIndex = 0;
-				  
+var answerNumber;	
+var questionCount = 0 ;	 
+var maxQuestions =5; 
+var clicked = false;
 
 $(document).ready(function() {
 
-  $("#btnNewGame").on("click", function() {
-  	if (gameStatus==="new"){
-  			$("#timeLeft").text("Time Remaining: 00:00");
-  			run();
-  			newQuestion();
-  			gameStatus = "inprogress";
-  	}
+$("#btnNewGame").on("click", function() {
+  	  newGame();
 });
 
-$("#btnEndGame").on("click", function() {
+$("#btnEndGame").on("click", function(e) {
+  	e.preventDefault();
   	stop();
+  	//setTimeout(reset, 5000,1,2);
 });
 
-$(".rb").on("click", function() {
+$(".rb").on("click", function(e) {
+  if (!clicked){
+  	clicked = true;
+	e.preventDefault();
+	stop();
   	if ($(this).attr("data-value") === "true"){
-  		alert("boom");
+  		correctAnswer();
+  	}																							
+  	else{
+  		wrongAnswer($(this).attr("label-index"),answerNumber);  		
+    }
+    disableRadioBtn(true);
+    if (questionCount === maxQuestions) {
+    	gameStatus = 'end';
+    	stop();
+    	//call gameStats();
+    }
+    else{
+	    setTimeout(reset, 5000,$(this).attr("label-index"),answerNumber);
+	    setTimeout(loadQuestion,5010);
+	    setTimeout(run,5010);
+  	}
+  }
+});
+
+function newGame(){
+
+   if (gameStatus==="new"){
+
+   		$("#timeLeft").text("Time Remaining : 00:00");
+   		run();
+		loadQuestion();
+		gameStatus = "inprogress";
   	}
   	else{
-  		alert("downnn");
+
   	}
-});
+}
 
 
-function newQuestion(){
+function loadQuestion(){
 	var j = 0;
 	$("#question").text(questions[qIndex].q);
-	var answerNumber = getRandomNumber();
+	answerNumber = getRandomNumber();
+	questionCount++;
 	for (i=1; i <5 ; i++){
-		console.log(answerNumber);
+
+		$("#a"+ i).attr("checked",false);
 		if (i === answerNumber){
 			$("#labela"+ answerNumber).text(questions[qIndex].rightAnswer);
 			$("#a"+answerNumber).removeClass("hidden");
 			$("#a"+answerNumber).attr("data-value","true");
+			$("#a"+answerNumber).attr("label-index",answerNumber);
+			$("#a"+answerNumber).attr("data-answer",questions[qIndex].rightAnswer);
+			//$("#a"+answerNumber).checked = true;
+			
 		}
 		else{
 			$("#labela"+ i).text(questions[qIndex].a[j]);
 			$("#a" + i).removeClass("hidden");
 			$("#a" + i).attr("data-value","false");
+			$("#a"+ i).attr("label-index",i);
+			$("#a"+ i).attr("data-answer",questions[qIndex].a[j]);
+			//$("#a" + i).checked = true;
 			j++;
 		}
 	}
+	disableRadioBtn(false);
+}
+
+function wrongAnswer(keyWrong, keyRight){
+	$("#diva" + keyWrong).addClass("loose");
+  	$("#diva" + keyRight).addClass("win");	
+  	$("#labela"+ keyWrong).text( $("#a"+keyWrong).attr("data-answer") + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + 'Wrong');
+  	$("#labela"+ keyRight).text($("#a"+keyRight).attr("data-answer") + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + 'Correct');
+
+}
+
+function correctAnswer(){
+	$("#diva" + answerNumber).addClass("win");
+  	$("#labela"+ answerNumber).text( $("#a"+ answerNumber).attr("data-answer") + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + 'Correct');
+}
+
+function timeIsUp(){
+	//alert("Time is Up!, See the correct answer after clicking ok");
+	//$("#diva" + answerNumber).prepend("<div>Time is Up!</div>");
+	stop();
+	disableRadioBtn(true);
+	$("#diva" + answerNumber).addClass("win");
+  	$("#labela"+ answerNumber).text( $("#a"+ answerNumber).attr("data-answer") + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + 'Time Is Up!'); // + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0' +'Thi is the correct answer');
+	    
+	if (questionCount === maxQuestions) {
+    	gameStatus = 'end';
+    	stop();
+    	//call gameStats();
+    }
+    else{
+	    setTimeout(reset, 5000,0,answerNumber);
+	    setTimeout(loadQuestion,5010);
+	    setTimeout(run,5010);
+  	}
+
 }
 
 function run() {
@@ -69,21 +143,43 @@ function count() {
    if (time < questionTime){
 	    time++;
 	    var currentTime = timeConverter(time);
-	    $("#timeLeft").text("Time Remaining: " + currentTime);
+	    $("#timeLeft").text("Time Remaining : " + currentTime);
 	}
 	else{
 		gameStatus = "timeisup" ;
-		//alert(the correct answer is )
+		timeIsUp();
 	}
  }
 
-function  reset() {
+function  reset(looseIndx,winIndx) {
+
     time = 0;
+   	if (qIndex === questions.length) {
+    	qIndex = 0;
+    }
+    else{
+    	qIndex++;
+    }
+    //gameStatus = 'new';
+    clicked = false;
+    //if (!looseIndx === 0){
+       $("#diva" + looseIndx).removeClass("loose");
+    //}
+  	$("#diva" + winIndx).removeClass("win");	
     $("#timeLeft").text("00:00");
+
 }
 
 function getRandomNumber() {
    return (Math.floor(Math.random() * 3) + 1)
+}
+
+
+function disableRadioBtn(val){
+	$("#a1").attr("disabled",val);
+    $("#a2").attr("disabled",val);
+    $("#a3").attr("disabled",val);
+    $("#a4").attr("disabled",val);
 }
 
 function timeConverter(t) {
